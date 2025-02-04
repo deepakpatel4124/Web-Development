493a6ee629e5b52b3c5dd0690f9d2a924ffd9ae6
deepakpatel4124 <deepakpatel4124@gmail.com>
2025-02-05 01:09:34 +0530

    upto model creation

--- airline/flights/views.py
+++ airline/flights/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Flight
 
 # Create your views here.
+def index(request):
+    flights = Flight.objects.all()
+    return render(request, "flights/index.html", {
+        "flights": flights
+    })
+    
+    
+def flight(request, flight_id):
+    flight = Flight.objects.get(pk=flight_id) 
+    return render(request, "flights/flight.html", {
+        "flight": flight
+    })
+    
